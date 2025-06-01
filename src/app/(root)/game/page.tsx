24ed9b3c89a9a72a2586f863d18823d16caeea14
Yuanzhi <leyi.wang23@imperial.ac.uk>
2025-06-01 15:34:42 +0100

    Add return button.

--- src/app/(root)/game/page.tsx
+++ src/app/(root)/game/page.tsx
@@ -1,6 +1,8 @@
 "use client"
 
 import dynamic from 'next/dynamic';
+import { Button } from "antd";
+import { useRouter } from "next/navigation";
 
 // Disable SSR for the Phaser component
 const PhaserGame = dynamic(() => import('@/components/PhaserGame'), {
@@ -9,9 +11,13 @@ const PhaserGame = dynamic(() => import('@/components/PhaserGame'), {
 });
 
 export default function GamePage() {
+  const router = useRouter();
   return (
     <div>
       <PhaserGame />
+      <Button
+        onClick={() => router.push("/")}
+      >Homepage</Button>
     </div>
   );
 }
