50e02bbe6816ef0f3e8821d4ee16e577f2c6a949
Yuanzhi <leyi.wang23@imperial.ac.uk>
2025-06-05 19:57:23 +0100

    Adjust size.

--- src/components/Television.tsx
+++ src/components/Television.tsx
@@ -188,8 +188,8 @@ export default function Television({onMount, sendMessage, playList, chatPanelVis
               position: "absolute",
               top: 0,
               left: 0,
-              width: "100%",
-              height: "100%",
+              width: "99%",
+              height: "99%",
             }}
             id="yt-player"
             ref={playerRef}
