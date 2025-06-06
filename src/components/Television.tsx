41c2f41ca12bcb972bb7835b911de2e734b8167b
Yuanzhi <leyi.wang23@imperial.ac.uk>
2025-06-06 04:58:55 +0100

    fix video load

--- src/components/Television.tsx
+++ src/components/Television.tsx
@@ -250,7 +250,9 @@ export default function Television({
       </Title>
       <Button
       type="primary"
-      onClick={() => router.push(`/lumiroom/${chatroomId}`)}
+      onClick={() =>
+        window.location.pathname = `/lumiroom/${chatroomId}`
+      }
       style={{
         position: "absolute",
         top: 16,
